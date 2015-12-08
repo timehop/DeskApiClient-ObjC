0d55e9ce66b3feccd330fa4ade32a015904d5ddb
Noel Artiles <nartiles@salesforce.com>
2015-12-08 10:54:00 -0600

    Remove synchronize and switch eTagCache to atomic NSDictionary.

--- DeskAPIClient/DeskAPIClient/DSAPIETagCache.m
+++ DeskAPIClient/DeskAPIClient/DSAPIETagCache.m
@@ -5,19 +5,19 @@
 //  Created by Desk.com on 12/20/13.
 //  Copyright (c) 2015, Salesforce.com, Inc.
 //  All rights reserved.
-//  
+//
 //  Redistribution and use in source and binary forms, with or without modification, are permitted provided
 //  that the following conditions are met:
-//  
+//
 //     Redistributions of source code must retain the above copyright notice, this list of conditions and the
 //     following disclaimer.
-//  
+//
 //     Redistributions in binary form must reproduce the above copyright notice, this list of conditions and
 //     the following disclaimer in the documentation and/or other materials provided with the distribution.
-//  
+//
 //     Neither the name of Salesforce.com, Inc. nor the names of its contributors may be used to endorse or
 //     promote products derived from this software without specific prior written permission.
-//  
+//
 //  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
 //  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
 //  PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
@@ -40,13 +40,13 @@
 
 @interface DSAPIETagCache()
 
-@property (nonatomic, strong) NSMutableDictionary *eTagCache;
+@property (strong) NSDictionary *eTagCache;
 
 - (NSURL *)applicationCachesDirectory;
 - (NSURL *)eTagPlistURL;
 - (BOOL)saveETagCache;
 - (void)loadETagCache;
-- (NSMutableDictionary *)newETagCache;
+- (NSDictionary *)newETagCache;
 - (NSURL *)URLWithAbsoluteURLString:(NSString *)absoluteURLString relativeToBaseOfURL:(NSURL *)baseOfURL;
 
 @end
@@ -81,7 +81,7 @@
 
 - (void)loadETagCache
 {
-    NSMutableDictionary *eTagCache = [[NSDictionary dictionaryWithContentsOfURL:self.eTagPlistURL] mutableCopy];
+    NSDictionary *eTagCache = [[NSDictionary dictionaryWithContentsOfURL:self.eTagPlistURL] mutableCopy];
     
     if (![eTagCache.allKeys containsObject:kVersionKey]) {
         // clear out old cache
@@ -93,14 +93,16 @@
 
 - (void)setETag:(NSString *)eTag forURL:(NSURL *)url nextPageURL:(NSURL *)nextPageURL
 {
-    @synchronized(self) {
-        if (!self.eTagCache) {
-            self.eTagCache = [self newETagCache];
-        }
-        NSDictionary *valueDictionary = @{kETagKey:eTag, kNextPageURLKey: nextPageURL.absoluteString ? nextPageURL.absoluteString : kNullValue};
-        [self.eTagCache setValue:valueDictionary forKey:url.absoluteString];
-        [self saveETagCache];
+    if (!self.eTagCache) {
+        self.eTagCache = [self newETagCache];
     }
+    NSDictionary *valueDictionary = @{kETagKey:eTag, kNextPageURLKey: nextPageURL.absoluteString ? nextPageURL.absoluteString : kNullValue};
+    
+    NSMutableDictionary *newCache = self.eTagCache.mutableCopy;
+    [newCache setValue:valueDictionary forKey:url.absoluteString];
+    self.eTagCache = newCache.copy;
+    
+    [self saveETagCache];
 }
 
 - (NSString *)eTagForURL:(NSURL *)url
@@ -150,10 +152,9 @@
     [self saveETagCache];
 }
 
-- (NSMutableDictionary *)newETagCache
+- (NSDictionary *)newETagCache
 {
-    NSMutableDictionary *newCache = [[NSMutableDictionary alloc] initWithCapacity:kNumEndpointsWithETags];
-    newCache[kVersionKey] = kVersionValue;
+    NSDictionary *newCache = @{ kVersionKey : kVersionValue };
     return newCache;
 }
 @end
